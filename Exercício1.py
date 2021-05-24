f97ac15929d3d02c49dec70640b5bcf10917ba62
IzaBenfato <izabelle.benfato@gmail.com>
2021-05-24 17:27:31 -0300

    Exercicio 1 - teste

---
+++ Exercício1.py
@@ -0,0 +1,6 @@
+#Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado
+
+side=float (input("input here the side value "))
+area = side * side
+
+print("the area of square is:", area)
